c07072dec7be9a23f6581ec221a7f24830a01c7a
PatrickARatcliff <pratcliff5@gmail.com>
2022-09-19 13:09:53 -0600

    add routes and server.js

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+//http://localhost:3001/api
+// Import our modular routers for notes
+const notesRouter = require('./notes/notes');
+
+const router = express();
+
+router.use('./api/notes', notesRouter);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const router = express();
+
+// Middleware for parsing JSON and urlencoded form data
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+router.use('/api', api);
+
+router.use(express.static('public'));
+
+// GET Route for homepage
+router.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+router.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+router.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
